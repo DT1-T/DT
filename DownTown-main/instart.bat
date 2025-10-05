0508ed429b513c3f8893c955f9ea870f074ba8c6
DT1-T <nasser.nzz.78@gmail.com>
2025-10-06 01:28:25 +0300

    Delete DownTown-main directory

--- DownTown-main/instart.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting ngrok tunnel on port 8080...
-ngrok.exe http 8080
-pause
--- DownTown-main/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-echo Running website using live-server...
-live-server --host=0.0.0.0 --port=8080
-pause
